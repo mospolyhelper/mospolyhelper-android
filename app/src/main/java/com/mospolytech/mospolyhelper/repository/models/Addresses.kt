60d030c27334823744fd3460596ad9fde87e1399
Andrey Bogomolov <tipapro@hotmail.com>
2020-06-02 18:06:53 +0300

    Addresses fragment fixs

--- app/src/main/java/com/mospolytech/mospolyhelper/repository/models/Addresses.kt
+++ app/src/main/java/com/mospolytech/mospolyhelper/repository/models/Addresses.kt
@@ -3,6 +3,6 @@ package com.mospolytech.mospolyhelper.repository.models
 import java.util.*
 
 data class Addresses(
-    val addresses: SortedMap<String, List<String>>,
+    val addresses: Map<String, List<String>>,
     val version: Int
 ): Map<String, List<String>> by addresses
