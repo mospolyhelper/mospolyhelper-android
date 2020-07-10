02c77c76bbc84eb88e7e746b4c881cf6b7e7e01a
Andrey Bogomolov <tipapro@hotmail.com>
2020-07-10 11:00:02 +0300

    Fix nav deadlines action and AssetProvider.kt moved to APP

--- app/src/main/java/com/mospolytech/mospolyhelper/App.kt
+++ app/src/main/java/com/mospolytech/mospolyhelper/App.kt
@@ -2,14 +2,27 @@ package com.mospolytech.mospolyhelper
 
 import android.app.Application
 import android.content.Context
+import android.content.res.AssetManager
+import java.io.InputStream
 
 class App : Application() {
     companion object {
         lateinit var context: Context
+
+        private lateinit var assets: AssetManager
+
+        fun getAsset(assetName: String): InputStream? {
+            return try {
+                assets.open(assetName)
+            } catch (e: Exception ) {
+                null
+            }
+        }
     }
 
     override fun onCreate() {
         super.onCreate()
         context = applicationContext
+        App.assets = assets
     }
 }
