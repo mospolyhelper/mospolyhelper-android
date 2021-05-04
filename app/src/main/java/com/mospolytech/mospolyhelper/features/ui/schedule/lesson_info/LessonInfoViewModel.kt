9872223e096b51e8c7a4f158ecb10d239b029c32
Andrey Bogomolov <tipapro@mail.ru>
2021-05-04 13:16:37 +0300

    Di fix

--- app/src/main/java/com/mospolytech/mospolyhelper/features/ui/schedule/lesson_info/LessonInfoViewModel.kt
+++ app/src/main/java/com/mospolytech/mospolyhelper/features/ui/schedule/lesson_info/LessonInfoViewModel.kt
@@ -4,6 +4,7 @@ import com.mospolytech.mospolyhelper.data.deadline.DeadlinesRepository
 import com.mospolytech.mospolyhelper.data.schedule.repository.LessonTagsRepositoryImpl
 import com.mospolytech.mospolyhelper.domain.schedule.model.Lesson
 import com.mospolytech.mospolyhelper.domain.schedule.model.LessonPlace
+import com.mospolytech.mospolyhelper.domain.schedule.repository.LessonTagsRepository
 import com.mospolytech.mospolyhelper.features.ui.common.Mediator
 import com.mospolytech.mospolyhelper.features.ui.common.ViewModelBase
 import com.mospolytech.mospolyhelper.features.ui.common.ViewModelMessage
@@ -11,7 +12,7 @@ import java.time.LocalDate
 
 class LessonInfoViewModel(
     mediator: Mediator<String, ViewModelMessage>,
-    val tagRepository: LessonTagsRepositoryImpl,
+    val tagRepository: LessonTagsRepository,
     private val deadlinesRepository: DeadlinesRepository
 ) : ViewModelBase(mediator, LessonInfoViewModel::class.java.simpleName) {
     companion object {
