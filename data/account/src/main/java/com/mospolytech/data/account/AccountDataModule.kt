701af9372af04cac1b3cf74d130ca411443a3999
Alexander <like81@yandex.ru>
2021-12-21 19:05:05 +0300

    init data and domain modules

---
+++ data/account/src/main/java/com/mospolytech/data/account/AccountDataModule.kt
@@ -0,0 +1,9 @@
+package com.mospolytech.data.account
+
+import org.koin.dsl.module
+import retrofit2.Retrofit
+
+val accountDataModule = module {
+//    single { get<Retrofit>().create(ScheduleService::class.java) }
+//    single<ScheduleRepository> { ScheduleRepositoryImpl(get()) }
+}
