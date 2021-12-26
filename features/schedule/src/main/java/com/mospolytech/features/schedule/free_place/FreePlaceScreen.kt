c5e3e54d3bf06b4f15d82a69065f62dd6aa98461
Andrey Bogomolov <tipapro@mail.ru>
2021-12-26 03:23:28 +0300

    add sliders in FreePlace

--- features/schedule/src/main/java/com/mospolytech/features/schedule/free_place/FreePlaceScreen.kt
+++ features/schedule/src/main/java/com/mospolytech/features/schedule/free_place/FreePlaceScreen.kt
@@ -1,18 +1,42 @@
 package com.mospolytech.features.schedule.free_place
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.RangeSlider
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.koin.androidx.compose.getViewModel
 
 @Composable
 fun FreePlaceScreen(viewModel: FreePlaceViewModel = getViewModel()) {
     val state by viewModel.state.collectAsState()
 
-    FreePlaceContent(state)
+    FreePlaceContent(
+        state,
+        viewModel::onDateRangeChange,
+        viewModel::onTimeRangeChange
+    )
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun FreePlaceContent(state: FreePlaceState) {
-
+fun FreePlaceContent(
+    state: FreePlaceState,
+    onDateRangeChange: (ClosedFloatingPointRange<Float>) -> Unit,
+    onTimeRangeChange: (ClosedFloatingPointRange<Float>) -> Unit
+) {
+    Column(Modifier.fillMaxSize().padding(horizontal = 20.dp)) {
+        Spacer(Modifier.height(10.dp))
+        Text(text = state.dateFrom.toString())
+        Text(text = state.dateTo.toString())
+        RangeSlider(values = state.datesPositionRange, onValueChange = onDateRangeChange)
+        Spacer(Modifier.height(10.dp))
+        Text(text = state.timeFrom.toString())
+        Text(text = state.timeTo.toString())
+        RangeSlider(values = state.timesPositionRange, onValueChange = onTimeRangeChange)
+    }
 }
--- features/schedule/src/main/java/com/mospolytech/features/schedule/free_place/FreePlaceViewModel.kt
+++ features/schedule/src/main/java/com/mospolytech/features/schedule/free_place/FreePlaceViewModel.kt
@@ -1,26 +1,52 @@
 package com.mospolytech.features.schedule.free_place
 
 import com.mospolytech.domain.schedule.model.place.Place
-import com.mospolytech.domain.schedule.model.place.PlaceFilters
 import com.mospolytech.domain.schedule.usecase.ScheduleUseCase
 import com.mospolytech.features.base.BaseMutator
 import com.mospolytech.features.base.BaseViewModel
 import java.time.LocalDate
+import java.time.LocalTime
+import java.time.temporal.ChronoUnit
+import kotlin.math.roundToLong
 
 class FreePlaceViewModel(
     private val useCase: ScheduleUseCase
 ) : BaseViewModel<FreePlaceState, FreePlaceMutator, Nothing>(
-    FreePlaceState(),
+    initState(),
     FreePlaceMutator()
 ) {
+    fun onDateRangeChange(dateRange: ClosedFloatingPointRange<Float>) {
+        mutateState {
+            setDateRange(dateRange)
+        }
+    }
 
+    fun onTimeRangeChange(dateRange: ClosedFloatingPointRange<Float>) {
+        mutateState {
+            setTimeRange(dateRange)
+        }
+    }
 }
 
 data class FreePlaceState(
-    val dateFrom: LocalDate = LocalDate.MIN,
-    val dateTo: LocalDate = LocalDate.MAX,
+    val dateFrom: LocalDate = LocalDate.now(),
+    val dateTo: LocalDate = LocalDate.now(),
+    val datesPositionRange: ClosedFloatingPointRange<Float> = 0f..1f,
+    val timeFrom: LocalTime = LocalTime.MIN,
+    val timeTo: LocalTime = LocalTime.MAX,
+    val timesPositionRange: ClosedFloatingPointRange<Float> = 0f..1f,
     val places: List<Place> = emptyList()
-)
+) {
+    companion object {
+        val minPerDay = LocalTime.MAX.toSecondOfDay() / 60
+        val totalDays = 100L
+    }
+}
+
+private fun initState() : FreePlaceState {
+    val dateTo = LocalDate.now().plusDays(FreePlaceState.totalDays)
+    return FreePlaceState(dateTo = dateTo)
+}
 
 class FreePlaceMutator : BaseMutator<FreePlaceState>() {
     fun setDateFrom(dateFrom: LocalDate) =
@@ -31,5 +57,31 @@ class FreePlaceMutator : BaseMutator<FreePlaceState>() {
     fun setDateTo(dateTo: LocalDate) =
         set(state.dateTo, dateTo) {
             copy(dateTo = it)
+        }
+
+    fun setDateRange(datesPositionRange: ClosedFloatingPointRange<Float>) =
+        set(state.datesPositionRange, datesPositionRange) {
+            copy(datesPositionRange = it)
+        }.then {
+            setDateFrom(LocalDate.now().plusDays((FreePlaceState.totalDays * datesPositionRange.start).roundToLong()))
+            setDateTo(LocalDate.now().plusDays((FreePlaceState.totalDays * datesPositionRange.endInclusive).roundToLong()))
+        }
+
+    fun setTimeFrom(timeFrom: LocalTime) =
+        set(state.timeFrom, timeFrom) {
+            copy(timeFrom = it)
+        }
+
+    fun setTimeTo(timeTo: LocalTime) =
+        set(state.timeTo, timeTo) {
+            copy(timeTo = it)
+        }
+
+    fun setTimeRange(timesPositionRange: ClosedFloatingPointRange<Float>) =
+        set(state.timesPositionRange, timesPositionRange) {
+            copy(timesPositionRange = it)
+        }.then {
+            setTimeFrom(LocalTime.MIN.plusMinutes((FreePlaceState.minPerDay * timesPositionRange.start).roundToLong()))
+            setTimeTo(LocalTime.MIN.plusMinutes((FreePlaceState.minPerDay * timesPositionRange.endInclusive).roundToLong()))
         }
 }
