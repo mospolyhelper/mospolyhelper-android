3078b9fbf227029a88b219f31a25d83fce708f85
Andrey Bogomolov <tipapro@mail.ru>
2022-01-05 03:54:48 +0300

    undo update kotlin 2

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,5 +11,5 @@ repositories {
 
 dependencies {
     implementation("com.android.tools.build:gradle:7.0.4")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
 }
